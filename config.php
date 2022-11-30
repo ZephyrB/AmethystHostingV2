b53e42d3c86cad8226e3c851696434142403353e
ZephyrB <zephyrb@zbycode.com>
2022-11-30 03:06:04 +0000

    Minor Changes

--- config.php
+++ config.php
@@ -32,7 +32,7 @@ if(!empty($_SERVER['HTTP_CLIENT_IP'])) {
     $ip = $_SERVER['REMOTE_ADDR'];
 }
 // Get Page
-if($_SERVER['REQUEST_URI'] != '/') {
+if($_SERVER['REQUEST_URI'] != '/' && $_SERVER['REQUEST_URI'] != '') {
     $p = explode('/', $_SERVER['REQUEST_URI'])[-1];
 }
 $tp = Array('score'=>'4.0','of'=>3)
